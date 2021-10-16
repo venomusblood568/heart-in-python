34e2799ed6468537d8917696c9ed386a96a92c5b
MR.JHA <90522738+venomusblood568@users.noreply.github.com>
2021-10-16 22:44:22 +0530

    main file

---
+++ heart.py
@@ -0,0 +1,14 @@
+from turtle import *  #WE ARE IMPORT TURTLE FROM ALL 
+color("red","black",)                    #assigning colour  
+begin_fill()                    # we are telling you function to fill    
+pensize(1)                         #we are assigning the size of pen
+left(50)                          #from liine 5 to line10  we are assigning the path the cursor is going to follow  
+forward(113)                     
+circle(50,200)                      
+right(140)
+circle(50,200)
+forward(133)                            
+end_fill()                     # we are saying that after drawing the shape you have to fill it  
+done()                           # are using done() function  cause we want that shapr to stay then disappear
+
+
